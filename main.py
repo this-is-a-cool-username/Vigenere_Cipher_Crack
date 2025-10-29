6b0e486882ade33fc64fdca88fd1f893b5b41d82
vadnaia <aaron@LAPTOP-0HM217M6.>
2025-10-28 17:21:04 -0700

    main

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #Blake Marshall, Isaac Tate & Aaron Vadnais
 #CISS 478
 #Team Challenge 1: Vigenere Cipher 
+#This program attempts to decipher a string that has been 
+#encoded using the Vigenere cipher.
+
 
