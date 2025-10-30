3c442d4a4e7fdeba89b5f93bb3a0285c0dcabd0b
Bahtahta <blakemarshall2021@hotmail.com>
2025-10-29 17:39:04 -0700

    Add files via upload
    python file is for the direct translation. takes a key and encrypted text and translates it

---
+++ ViginereCipher.py
@@ -0,0 +1,29 @@
+cipherKey = ""
+keyArray = []
+cipherText = ""
+keyLength = len(cipherKey) - 1
+
+if keyLength < 0: # Error handler
+    print("Error: No key")
+    exit(1)
+
+for c in cipherKey: # coverts the cipher key into an array
+    keyArray.append(c)
+
+def solver(cipher, key):
+    plain = ""
+    counter = 0
+    cipher = cipher.upper()
+    for s in cipher:
+        cipherVal = ord(s) - ord('A')
+        keyVal = ord(key[counter]) - ord('A')
+        written = (cipherVal - keyVal) % 26
+        counter += 1
+        if counter > keyLength:
+            counter = 0
+        plain = plain + chr(written + ord('A'))
+        #print(f"Cipher: {s}, Key letter: {key[counter]}, Counter: {counter}")
+
+    print(plain)
+
+
