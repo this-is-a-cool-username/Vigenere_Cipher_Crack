7cbd96b976631e99a9d543f7a41536ba7ed6a7d6
Bahtahta <blakemarshall2021@hotmail.com>
2025-10-31 05:39:19 -0700

    Update ViginereCipher.py
    added friedman test. needs additional work to narrow results and find the key. almost done. i will get it this morning i think

--- ViginereCipher.py
+++ ViginereCipher.py
@@ -27,4 +27,38 @@ def solver(cipher, key):
     print(plain)
 
 
+Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+EnglishFreq = [ 8.2, 1.5, 2.8, 4.3, 12.7, 2.2, 2.0, 6.1, 7.0, 0.15, 0.77, 4.0, 2.4, 6.7, 7.5, 1.9, 0.095, 6.0, 6.3, 9.1, 2.8, 0.98, 2.4, 0.15, 2.0, 0.074 ] 
+EnglishFreqPerc = [f/ 100.0 for f in EnglishFreq]
+
+def indexOfCo(string: str) -> float:
+    num = len(string)
+    count = [0] * 26
+    for c in string:
+        count[ord(c) - ord('A')] += 1
+    numer = sum(c * (c - 1) for c in count)
+    return numer / (num * (num -1))
+
+def friedmanTest(cipher: str):
+    cipher = cipherText
+    cNum = len(cipher)
+    iC = indexOfCo(cipher)
+    kr = 0.0385
+    kp = 0.067
+    k = kp - kr
+
+    denomiator = ((cNum - 1) * iC) - (kr * cNum) + kp
+    estimate = (k * cNum) / denomiator
+
+    test = max(1, int(round(estimate)))
+
+#    print(f"Friedman Test:")
+#    print(f"  Length (n): {cNum}")
+#    print(f"  Index of Coincidence (IC): {iC}")
+#    print(f"  Estimated key length (float): {estimate}")
+#    print(f"  Estimated key length (int): {test}")
+
+    return test
+
+friedmanTest(cipherText)
 
